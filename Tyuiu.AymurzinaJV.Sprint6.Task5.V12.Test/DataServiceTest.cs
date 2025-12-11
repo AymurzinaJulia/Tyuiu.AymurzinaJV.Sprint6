0385aac8709fc43869f0c8540679c6e0419d856e
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-12-12 02:00:30 +0500

    Task5 added

--- Tyuiu.AymurzinaJV.Sprint6.Task5.V12.Test/DataServiceTest.cs
+++ Tyuiu.AymurzinaJV.Sprint6.Task5.V12.Test/DataServiceTest.cs
@@ -1,4 +1,5 @@
-﻿namespace Tyuiu.AymurzinaJV.Sprint6.Task5.V12.Test
+﻿using Tyuiu.AymurzinaJV.Sprint6.Task5.V12.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint6.Task5.V12.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -6,6 +7,12 @@
         [TestMethod]
         public void TestMethod1()
         {
+            string fullPath = @"C:\DataSprint6\InPutDataFileTask5V12.txt";
+
+            FileInfo fileInfo = new FileInfo(fullPath);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
         }
     }
 }
